f69320878985fc86a3e1a4082053aee2316935e1
Ghs267 <hariyantosetiadi@gmail.com>
2021-04-17 23:10:48 +0700

    1.5 done

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -4,27 +4,29 @@ import Content from './Content'
 import Total from './Total'
 
 const App = ()=>{
-  const course = 'Half Stack application development'
-  const part =[
-    {
-      name : 'Fundamental of React',
-      exercise : 10
-    },
-    {
-      name : 'Using props to pass data',
-      exercise : 7
-    },
-    {
-      name : 'State of component',
-      exercise : 14
-    }
-  ]
+  const course ={
+    name : 'Half Stack application development',
+    part : [
+      {
+        name : 'Fundamental of React',
+        exercise : 10
+      },
+      {
+        name : 'Using props to pass data',
+        exercise : 7
+      },
+      {
+        name : 'State of component',
+        exercise : 14
+      }
+    ]
+  } 
   
   return(
     <div>
-      <Header course={course} />
-      <Content parts={part}/>
-      <Total parts={part} />
+      <Header course={course.name} />
+      <Content parts={course.part}/>
+      <Total parts={course.part} />
     </div>
   )
 }
